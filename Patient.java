1c1db4a9ead84733d6e09930b13bccc89f87b675
unknown <aho04@cspc113.dcs.bbk.ac.uk>
2013-11-11 20:01:44 +0000

    Patient class

---
+++ Patient.java
@@ -0,0 +1,35 @@
+public class Patient {
+	private String name;
+	private int age;
+	private String illness;
+	Patient nextPatient;
+
+	public Patient(String name,int age,String illness){
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null;
+	}
+	public void addPatient(Patient newPatient){
+		if(this.nextPatient == null){
+			this.nextPatient = newPatient;
+		} else {
+			this.nextPatient.addPatient(newPatient);
+		}
+	}
+	public boolean deletePatient(Patient patient){
+		if(this.nextPatient == null){
+			return false;
+		} else if(this.nextPatient.name.equals(patient.name)){
+			this.nextPatient = nextPatient.nextPatient;
+			return true;
+		} else {
+			return this.nextPatient.deletePatient(patient);
+		}
+	}
+	public void printPatient(){
+		System.out.println("Patient Name: " + this.name);
+		System.out.println("Patient Age: " + this.age);
+		System.out.println("Patient Illness: " + this.illness);
+	}
+}
